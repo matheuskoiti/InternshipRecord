338300e504f991bb1f49d3498c6403012a8ad5d8
Matheus Koiti Koyama da Silva <koiti.silva@gmail.com>
2017-02-24 09:24:30 -0300

    making records list show at main activity

--- app/src/main/java/com/internrecord/android/mkoyama/MainActivity.java
+++ app/src/main/java/com/internrecord/android/mkoyama/MainActivity.java
@@ -1,6 +1,7 @@
 package com.internrecord.android.mkoyama;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,9 +9,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.internrecord.android.mkoyama.db.RecordContract;
 import com.internrecord.android.mkoyama.db.RecordDbHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,9 +30,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         ListView lv_records = (ListView) findViewById(R.id.lv_records);
-
-        RecordDbHelper dbHelper = new RecordDbHelper(this);
-        mdb = dbHelper.getWritableDatabase();
+        List recordList = loadRecordsData();
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, recordList);
+        lv_records.setAdapter(adapter);
 
         setSupportActionBar(toolbar);
     }
@@ -40,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -51,5 +57,29 @@ public class MainActivity extends AppCompatActivity {
     public void onClickFloatingButton(View view) {
         Intent addRecordActivity = new Intent(this, AddRecord.class);
         startActivity(addRecordActivity);
+    }
+
+    private List loadRecordsData() {
+        RecordDbHelper dbHelper = new RecordDbHelper(this);
+        mdb = dbHelper.getReadableDatabase();
+
+        Cursor cursor = mdb.query(
+                RecordContract.RecordEntry.TABLE_NAME,  // The table to query
+                null,                                   // The columns to return
+                null,                                   // The columns for the WHERE clause
+                null,                                   // The values for the WHERE clause
+                null,                                   // don't group the rows
+                null,                                   // don't filter by row groups
+                null                                    // The sort order
+        );
+
+        List itemSummaries = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            String summ = cursor.getString(cursor.getColumnIndex(RecordContract.RecordEntry.COLUMN_SUMMARY));
+            itemSummaries.add(summ);
+        }
+        cursor.close();
+
+        return itemSummaries;
     }
 }
