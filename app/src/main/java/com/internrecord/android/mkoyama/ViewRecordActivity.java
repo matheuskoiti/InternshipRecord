e214f143846eb62dfdb03f5c9c9a61805ee19a33
Matheus Koiti Koyama da Silva <koiti.silva@gmail.com>
2017-02-24 11:01:57 -0300

    creating record deletion

--- app/src/main/java/com/internrecord/android/mkoyama/ViewRecordActivity.java
+++ app/src/main/java/com/internrecord/android/mkoyama/ViewRecordActivity.java
@@ -1,10 +1,15 @@
 package com.internrecord.android.mkoyama;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.internrecord.android.mkoyama.db.RecordContract;
+import com.internrecord.android.mkoyama.db.RecordDbHelper;
 
 public class ViewRecordActivity extends AppCompatActivity {
 
@@ -12,6 +17,7 @@ public class ViewRecordActivity extends AppCompatActivity {
     private TextView tv_summay_view;
     private TextView tv_desc_view;
     private TextView tv_week_view;
+    private SQLiteDatabase mdb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +36,19 @@ public class ViewRecordActivity extends AppCompatActivity {
         tv_week_view.setText(recordItem.getWeek());
     }
 
+    // Remove a Record from database
+    public void onClickBtnDelete(View view) {
+        if (removeRecord()) {
+            Toast.makeText(getApplicationContext(), "Registro removido com sucesso", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Não foi possível remover o registro", Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    private boolean removeRecord() {
+        RecordDbHelper dbHelper = new RecordDbHelper(this);
+        mdb = dbHelper.getReadableDatabase();
+        long id = recordItem.getId();
+        return mdb.delete(RecordContract.RecordEntry.TABLE_NAME, RecordContract.RecordEntry._ID + "=" + id, null) > 0;
+    }
 }
